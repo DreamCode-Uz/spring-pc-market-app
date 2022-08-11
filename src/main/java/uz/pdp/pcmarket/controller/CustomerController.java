3dc0fc5011c28845ab4fe9a214b654b966c341aa
Dilshod Fayzullayev <darkprohub-uz@yandex.com>
2022-08-11 16:42:31 +0500

    update project

---
+++ src/main/java/uz/pdp/pcmarket/controller/CustomerController.java
@@ -0,0 +1,46 @@
+package uz.pdp.pcmarket.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.pdp.pcmarket.payload.CustomerDTO;
+import uz.pdp.pcmarket.service.CustomerService;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/customer")
+public class CustomerController {
+    private final CustomerService service;
+
+    @Autowired
+    public CustomerController(CustomerService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> customers(@RequestParam(name = "page", defaultValue = "1") Integer page,
+                                       @RequestParam(name = "size", defaultValue = "1") Integer size) {
+        return service.getAll(page, size);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> customer(@PathVariable Integer id) {
+        return service.getOne(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> customer(@Valid @RequestBody CustomerDTO dto) {
+        return service.addCustomer(dto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Integer id, @Valid @RequestBody CustomerDTO dto) {
+        return service.editCustomer(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Integer id) {
+        return service.deleteCustomer(id);
+    }
+}
---
+++ src/main/java/uz/pdp/pcmarket/controller/ProductController.java
@@ -0,0 +1,46 @@
+package uz.pdp.pcmarket.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.pdp.pcmarket.payload.ProductDTO;
+import uz.pdp.pcmarket.service.ProductService;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+    private final ProductService service;
+
+    @Autowired
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> customers(@RequestParam(name = "page", defaultValue = "1") Integer page,
+                                       @RequestParam(name = "size", defaultValue = "1") Integer size) {
+        return service.getAll(page, size);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> customer(@PathVariable Integer id) {
+        return service.getOne(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> customer(@Valid @RequestBody ProductDTO dto) {
+        return service.addProduct(dto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Integer id, @Valid @RequestBody ProductDTO dto) {
+        return service.editProduct(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Integer id) {
+        return service.delete(id);
+    }
+}
